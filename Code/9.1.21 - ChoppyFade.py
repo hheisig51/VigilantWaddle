dce897fee33a36709c6b6f5130e4ddab5911d7dd
Henry Heisig <hheisig51@ccs.local>
2021-09-10 09:49:09 -0400

    Added Neopixel code and video, will add additional videos later

---
+++ Code/9.1.21 - ChoppyFade.py
@@ -0,0 +1,28 @@
+# Write your code here :-)
+import board
+import neopixel
+import time
+
+x = 50
+y = 50
+z = 50
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+
+dot.brightness = .1
+
+while True:
+    dot.fill((x, y, z))
+    time.sleep(.25)
+    x = x + 1
+    if x >= 255:
+        x = 50
+
+    y = y + 5
+    if y >= 255:
+        y = 50
+
+    z = z + 10
+    if z >= 255:
+        z = 50
+
+    print((x, y, z))
